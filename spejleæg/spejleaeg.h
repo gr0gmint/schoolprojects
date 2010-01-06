a49533fc17a7b00836e8d61702cde4a35783703c
kris <kris@sagan.(none)>
2010-01-06 15:13:11 +0100

    lulcats

---
+++ spejleæg/spejleaeg.h
@@ -0,0 +1,21 @@
+#include <gloox/gloox.h>
+#include <gloox/presence.h>
+#include <gloox/client.h>
+#include <gloox/connectionlistener.h>
+#include <gloox/messagesession.h>
+#include <gloox/uniquemucroom.h>
+
+
+using namespace gloox;
+
+class SpejleaegPresence : public PresenceHandler
+{
+    public:
+        virtual void handlePresence( const Presence& presence );s
+}
+class SpejleaegBot : public ConnectionListener
+{
+    public:
+        virtual void onConnect();
+        virtual bool onTLSConnect( const CertInfo& info);
+}
